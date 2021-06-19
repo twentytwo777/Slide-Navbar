f087679d1c87383c744be500c2c302891ce45981
twenty two <51849001+twentytwo777@users.noreply.github.com>
2021-06-19 19:52:08 +0400

    Upload navbar

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+
+/* massive buttons */
+var navbar__toggle = document.querySelector('.navbar__toggle');
+var navbar_ml = document.querySelector('.navbar_ml');
+var logo___ml = document.querySelector('.logo___ml');
+
+navbar__toggle.addEventListener('click', function(){
+    navbar_ml.classList.toggle('active');
+});
+
+
+// hide menu after click on random place
+$(document).mouseup(function (e){
+    var div = $(".navbar_ml");
+    if (!div.is(e.target) 
+        && div.has(e.target).length === 0) { 
+        $('.navbar_ml').removeClass('active');
+    }
+});
