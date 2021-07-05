cdf769dd81dd6fcf043e69be7444a04879ae8f9e
twenty two <51849001+twentytwo777@users.noreply.github.com>
2021-07-05 11:37:23 +0400

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,18 +3,17 @@
 var navbar__toggle = document.querySelector('.navbar__toggle');
 var navbar_ml = document.querySelector('.navbar_ml');
 var logo___ml = document.querySelector('.logo___ml');
-
+var secondlogo = document.querySelector('.logo_hide_pc');
+var secondtext = document.querySelector('.hide_txt_pc');
+var button_hide_pc = document.querySelector('.button_hide_pc');
 
 navbar__toggle.addEventListener('click', function(){
-    navbar_ml.classList.toggle('active');
+    navbar_ml.classList.add('active');
+    secondlogo.classList.add('activef');
+    secondtext.classList.add('activef');
+    button_hide_pc.classList.add('activef');
 });
 
-
-// hide menu after click on random place
-$(document).mouseup(function (e){
-    var div = $(".navbar_ml, .navbar__toggle");
-    if (!div.is(e.target) 
-        && div.has(e.target).length === 0) { 
-        $('.navbar_ml, .navbar__toggle').removeClass('active');
-    }
+button_hide_pc.addEventListener('click', function(){
+    navbar_ml.classList.remove('active');
 });
